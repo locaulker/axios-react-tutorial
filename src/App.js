256e85d1dc67c7b08fc06f45356487fe3fef0f89
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-21 17:20:22 -0500

    implemented get request with headers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Heading from "./components/Heading"
-import Setup from "./examples/1-first-request"
+import Setup from "./examples/2-headers"
 
 function App() {
   return (
--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -1,23 +1,33 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react"
+import axios from "axios"
 
-const url = 'https://icanhazdadjoke.com/';
+const url = "https://icanhazdadjoke.com/"
 // Accept : 'application/json'
 
 const Headers = () => {
-  const [joke, setJoke] = useState('random dad joke');
+  const [joke, setJoke] = useState("random dad joke")
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
-  };
+    try {
+      const { data } = await axios.get(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      setJoke(data.joke)
+      console.log(data.joke)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
 
   return (
-    <section className='section text-center'>
-      <button className='btn' onClick={fetchDadJoke}>
+    <section className="section text-center">
+      <button className="btn" onClick={fetchDadJoke}>
         random joke
       </button>
-      <p className='dad-joke'>{joke}</p>
+      <p className="dad-joke">{joke}</p>
     </section>
-  );
-};
-export default Headers;
+  )
+}
+export default Headers
